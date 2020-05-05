94f0a41a6373f13dd57b3cef7dc956e1833383ba
Sara Pettigrew <sarapettigrew@Saras-MacBook-Pro.local>
2020-05-05 16:50:05 -0600

    More items and group added

--- dnd/src/App.js
+++ dnd/src/App.js
@@ -5,7 +5,7 @@ import DragDrop from "./components/DragDrop"
 const data = [
     {title: 'Group 1', items: ['Item 1', 'Item 2', 'Item 3']},
     {title: 'Group 2', items: ['Item 4', 'Item 5']},
-
+    {title: 'Group 3', items: ['Item 6', 'Item 7', 'Item 8']},
 ]
 
 function App() {
