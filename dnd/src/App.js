8b7f8571939bc6d04ad8499026143bd4e266bba3
Sara Pettigrew <sarapettigrew@Saras-MacBook-Pro.local>
2020-05-05 11:00:51 -0600

    Added component to file to lessen file size

--- dnd/src/App.js
+++ dnd/src/App.js
@@ -1,9 +1,10 @@
 import React from "react"
 import './App.css'
+import DragDrop from "./components/DragDrop"
 
 const data = [
-    {title: 'group 1', items: ['1', '2', '3']},
-    {title: 'group 2', items: ['4', '5']},
+    {title: 'Group 1', items: ['Item 1', 'Item 2', 'Item 3']},
+    {title: 'Group 2', items: ['Item 4', 'Item 5']},
 
 ]
 
@@ -11,46 +12,7 @@ function App() {
     return (
         <div className='App'>
             <header className='App-header'>
-                <div className='drag-drop'>
-                {data.map((grp, grpI) => (
-                  <div key={grp.title} className='dnd-group'>
-                    {grp.items.map((item, itemI) => (
-                        <div draggable key={item} className='dnd-item'>
-                        {item}
-                        </div>
-                    ))}
-                    </div> 
-                ))}
-                </div> 
-
-                <div className='drag-drop'> 
-                <div className="dnd-group"> 
-                <div className="group-title">Group 1</div>
-                <div className="dnd-item">
-                    <div>
-                    <p>Item 1</p>
-                    </div>
-                </div>
-                <div className="dnd-item">
-                    <div>
-                    <p>Item 2</p>
-                    </div>
-                </div>
-                </div>
-                <div className="dnd-group"> 
-                <div className="group-title">Group 2</div>
-                <div className="dnd-item">
-                    <div>
-                    <p>Item 1</p>
-                    </div>
-                </div>
-                <div className="dnd-item">
-                    <div>
-                    <p>Item 2</p>
-                    </div>
-                </div>
-                </div>
-                </div>
+            <DragDrop data={data}/>
             </header>
         </div>
     )
